804d4f90be147890483306dee5c7558c96d2d996
hoanggia2412 <62107745+hoanggia2412@users.noreply.github.com>
2021-10-18 23:31:24 +0700

    Update port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = process.env.port
+const port = process.env.PORT
 const publicDirectoryPath = path.join(__dirname,'../public')
 const Filter =  require('bad-words')
 const { generateMessages, generateLocationMessages } = require('./utils/messages')
